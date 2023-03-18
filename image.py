1496370f2ce9a4c7da43fd4309711e483e12a9bb
grenn24 <hoodiheng@gmail.com>
2023-03-19 00:28:33 +0800

    updated image.py to iterate, process and export each image

--- image.py
+++ image.py
@@ -7,3 +7,11 @@ counter = 1
 for i in range(len(pdf)):    #iterate over each page in pdf
     page = pdf[i]
     images = page.get_images()
+    for image in images:     #iterate over each extracted image
+        base_img = pdf.extract_image(image[0])
+        print(base_img)     #display image metadata
+        image_data = base_img["image"]     #extract image data
+        img = PIL.Image.open(io.BytesIO(image_data))     #process image data
+        ext = base_img["ext"]     #extract image extension
+        img.save(open(f"image{counter}.{ext}", "wb"))     #save image as readable file
+        counter += 1     #repeat for subsequent images
