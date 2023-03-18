f116e54da03d2f40f0b97a1a74625de4748dc130
grenn24 <hoodiheng@gmail.com>
2023-03-18 23:57:15 +0800

    added pattern recognition feature to text extractor

--- text.py
+++ text.py
@@ -1,5 +1,11 @@
 import re
 from pdfminer.high_level import extract_pages, extract_text
 
-text = extract_text("sample.pdf")
+#extract all text from pdf file
+text = extract_text("sample1.pdf")
 print(text)
+
+#compile and extract text with specific patterns
+pattern = re.compile(r"[a-zA-Z]+,{1}\s{1}")
+matches = pattern.findall(text)
+print(matches)
