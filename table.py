33207ce8be254b1cb214bd01cdffbdf74ec50a36
grenn24 <hoodiheng@gmail.com>
2023-03-19 00:48:54 +0800

    updated table extractor feature

--- table.py
+++ table.py
@@ -2,4 +2,4 @@ import tabula
 
 tables = tabula.read_pdf("sample.pdf", pages="4")     #select pages to read
 dataframe = tables[0]     #display table information
-print(dataframe)
+print(dataframe[dataframe.India > 50])     #filter table data to display
