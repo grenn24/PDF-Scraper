1add870421bdd5613f55377f758b2598ce7003a4
grenn24 <hoodiheng@gmail.com>
2023-03-19 12:02:52 +0800

    added .txt saving feature for tables

--- table.py
+++ table.py
@@ -2,4 +2,9 @@ import tabula
 
 tables = tabula.read_pdf("sample.pdf", pages="4")     #select pages to read
 dataframe = tables[0]     #display table information
-print(dataframe[dataframe.India > 50])     #filter table data to display
+print(dataframe[dataframe.Japan > 5])     #filter table data to display
+
+#save extracted tables into .txt file
+f = open("table.txt", "w")
+f.write(str(dataframe))
+f.close()
